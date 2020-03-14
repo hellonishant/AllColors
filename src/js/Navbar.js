550aff328a9617bb5f8740c53b844bb7f89f54cd
Nishant <menishant77@gmail.com>
2020-03-14 05:44:06 +0530

    Added snackbar and styled Navbar

--- src/js/Navbar.js
+++ src/js/Navbar.js
@@ -2,20 +2,25 @@ import React, { Component } from "react";
 import Slider from "rc-slider";
 import Select from "@material-ui/core/Select";
 import MenuItem from "@material-ui/core/MenuItem";
+import Snackbar from "@material-ui/core/Snackbar";
+import IconButton from "@material-ui/core/IconButton";
+import CloseIcon from "@material-ui/icons/Close";
 import "rc-slider/assets/index.css";
 import "../css/Navbar.css";
 
 class Navbar extends Component {
 	constructor(props) {
 		super();
-		this.state = { format: "hex" };
+		this.state = { format: "hex", openSnackbar: false };
 	}
 
-	handleChange = event => {
-		this.setState({ format: event.target.value }, () =>
+	handleFormatChange = event => {
+		this.setState({ format: event.target.value, openSnackbar: true }, () =>
 			this.props.handleFormatChange(this.state.format)
 		);
 	};
+
+	closeSnackbar = () => this.setState({ openSnackbar: false });
 	render() {
 		let { level, changeLevel } = this.props;
 		return (
@@ -33,12 +38,34 @@ class Navbar extends Component {
 					/>
 				</div>
 				<div className="select-container">
-					<Select value={this.state.format} onChange={this.handleChange}>
+					<Select value={this.state.format} onChange={this.handleFormatChange}>
 						<MenuItem value="hex">hex #ffffff</MenuItem>
 						<MenuItem value="rgb">rgb(255, 255, 255)</MenuItem>
 						<MenuItem value="rgba">rgba(255, 255, 255, 1.0)</MenuItem>
 					</Select>
 				</div>
+				<Snackbar
+					anchorOrigin={{ vertical: "bottom", horizontal: "left" }}
+					open={this.state.openSnackbar}
+					autoHideDuration={2000}
+					onClose={this.closeSnackbar}
+					message={
+						<span id="message-id">
+							Format Changed to {this.state.format.toUpperCase()}
+						</span>
+					}
+					ContentProps={{ "aria-describedby": "message-id" }}
+					action={[
+						<IconButton
+							onClick={this.closeSnackbar}
+							color="inherit"
+							key="close"
+							aria-label="close"
+						>
+							<CloseIcon />
+						</IconButton>
+					]}
+				/>
 			</header>
 		);
 	}
