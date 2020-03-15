9b2c18d2c322b15fb797d1117af3494a0905a778
Nishant <menishant77@gmail.com>
2020-03-15 23:58:10 +0530

    Added slider styles

--- src/styles/navbarStyles.js
+++ src/styles/navbarStyles.js
@@ -23,7 +23,23 @@ export default {
 	Slider: {
 		width: "340px",
 		margin: "0 10px",
-		display: "inline-block"
+		display: "inline-block",
+		"& .rc-slider-track": {
+			backgroundColor: "transparent"
+		},
+		"& .rc-slider-rail": {
+			height: "8px"
+		},
+		"& .rc-slider-handle, .rc-slider-handle:active, .rc-slider-handle:focus, .rc-slider-handle:hover": {
+			backgroundColor: "green",
+			outline: "none",
+			border: "2px solid green",
+			boxShadow: "none",
+			width: "13px",
+			height: "13px",
+			marginLeft: "-7px",
+			marginTop: "-3px"
+		}
 	},
 
 	selectContainer: {
