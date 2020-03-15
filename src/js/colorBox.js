e2e15ff9e3947e98e8f053ba7b537a5a50a80d39
Nishant <menishant77@gmail.com>
2020-03-15 05:30:04 +0530

    Added dynamic text colors

--- src/js/colorBox.js
+++ src/js/colorBox.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { CopyToClipboard } from "react-copy-to-clipboard";
 import { Link } from "react-router-dom";
+import chroma from "chroma-js";
 import "../css/colorBox.css";
 
 class ColorBox extends Component {
@@ -16,8 +17,17 @@ class ColorBox extends Component {
 			}, 1500);
 		});
 	};
+
+	getColor = background => {
+		let white = chroma.contrast("#eeeeee", background);
+		let black = chroma.contrast(background, "#222222");
+		if (white > black) return "#eee";
+		return "#222";
+	};
+
 	render() {
 		const { background, name, moreUrl, link } = this.props;
+		let textColor = this.getColor(background);
 		return (
 			<CopyToClipboard text={background} onCopy={this.copyAnimationFunc}>
 				<div className="ColorBox" style={{ backgroundColor: background }}>
@@ -27,16 +37,20 @@ class ColorBox extends Component {
 						style={{ backgroundColor: background }}
 					/>
 					<div className={`Copy-text ${this.state.copyAnimation && "show"}`}>
-						<h1>Copied!</h1>
-						<span>{background}</span>
+						<h1 style={{ color: textColor }}>Copied!</h1>
+						<span style={{ color: textColor }}>{background}</span>
 					</div>
-					<button className="Copy-button">Copy</button>
+					<button className="Copy-button" style={{ color: textColor }}>
+						Copy
+					</button>
 					<div className="ColorBox-name">
-						<span>{name}</span>
+						<span style={{ color: textColor }}>{name}</span>
 					</div>
 					{link && (
 						<Link to={moreUrl} onClick={event => event.stopPropagation()}>
-							<span className="ColorBox-more">More</span>
+							<span className="ColorBox-more" style={{ color: textColor }}>
+								More
+							</span>
 						</Link>
 					)}
 				</div>
